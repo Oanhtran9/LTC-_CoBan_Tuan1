e80bf88a9826f959d54bc7f17fc42bb8850ecef8
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-01-29 19:02:35 +0700

    bai1_tuan1

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Họ tên: Trần Thị Oanh");
+        }
+    }
+}
